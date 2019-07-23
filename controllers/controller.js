88eba1a0cf885edf695ba7bce1592053a38578d6
Megha Prasad Bindiganavale <meghab.prasad@gmail.com>
2019-07-23 16:42:46 -0700

    add scripts.js for jquery functionality

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,156 +2,149 @@ const express = require('express');
 const cheerio = require('cheerio');
 const db = require('../models/');
 const axios = require("axios");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/techNews", { useNewUrlParser: true });
 
 // Initialize Express
-const app = express();
+// const app = express();
+var router = express.Router();
 
 
-app.get("/scrape", function (req, res) {
-    // Make a request via axios for the news section of `ycombinator`
-    axios.get("https://news.ycombinator.com/").then(function (response) {
-        // Load the html body from axios into cheerio
-        var $ = cheerio.load(response.data);
-        // For each element with a "title" class
-        $(".title").each(function (i, element) {
-            // Save the text and href of each link enclosed in the current element
-            let result = {};
-            result.title = $(element).children("a").text();
-            result.link = $(element).children("a").attr("href");
-
-
-
-            // If this found element had both a title and a link
-            if (result.title && result.link) {
-                // Insert the data in the scrapedData db
-                db.Article
-                    .create(result)
-                    .then(function (dbArticle) {
-                        res.send();
-                        console.log(dbArticle);
-                    })
-                    .catch(function (err) {
-                        res.json(err);
-                        console.log(err);
-                    });
-            };
+    router.get("/scrape", function (req, res) {
+        // Make a request via axios for the news section of `ycombinator`
+        axios.get("https://news.ycombinator.com/").then(function (response) {
+            // Load the html body from axios into cheerio
+            var $ = cheerio.load(response.data);
+            // For each element with a "title" class
+            $(".title").each(function (i, element) {
+                // Save the text and href of each link enclosed in the current element
+                let result = {};
+                result.title = $(element).children("a").text();
+                result.link = $(element).children("a").attr("href");
+    
+    
+    
+                // If this found element had both a title and a link
+                if (result.title && result.link) {
+                    // Insert the data in the scrapedData db
+                    db.Article
+                        .create(result)
+                        .then(function (dbArticle) {
+                            res.send();
+                            console.log(dbArticle);
+                        })
+                        .catch(function (err) {
+                            res.json(err);
+                            console.log(err);
+                        });
+                };
+            });
         });
     });
-});
-
-//retrieve data from our mongodb  (database: techNews, collection: Article) 
-//gets all info about article including related saves and comments
-
-app.get('/', (req, res) => {
-    db.Article
-        //find all the articles in the collection  
-        .find({})
-        //sort the articles by date scraped
-        .sort({dateScraped: -1})
-        //get another collection related to the articles
-        .populate('note')
-        //once you have all the info
-        .then(dbArticle => {
-            //send the info to index
-            res.render('index', { data: dbArticle });
-        })
-        //if there is an error, send the error to the client
-        .catch(err => res.json(err));
-});
-
-//save articles that you like 
-app.post('/saved', function (req, res) {
-    db.Article.update(
-        { _id: req.params.id },
-        { $set: { saved: true } },
-        function (err, data) {
-            if (err) {
-                throw err;
-            }
-            res.send("Saved!")
-        }
-    );
-});
-
-router.get("/saved", function (req, res) {
-    db.Article.find({
-        saved: true
-    }, function (error, articles) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.render("saved", {
-                data: articles
-            });
-        }
-    })
-});
-
-//unsave articles 
-app.post('/unsave/:id', function (req, res) {
-    db.Article.update(
-        { _id: req.params.id },
-        { $set: { saved: false } },
-        function (err, data) {
-            if (err) {
-                throw err;
-            }
-            res.send("Unsaved!")
-        }
-    );
-});
-
-//add a comment/note
-app.post('/add/:id', function (req, res) {
-    db.Note
-        //create note with body of the comment or note
-        .create(req.body)
-        //once you have created that, 
-        .then(dbNote => {
-            //update the Article db with the note collection
-            db.Article.update(
-                { _id: req.params.id },
-                { $set: { note: dbNote._id } },
-                { multi: true },
-                (err, num) => {
-                    if (err) throw err;
-                    res.send('Added');
+    
+    //retrieve data from our mongodb  (database: techNews, collection: Article) 
+    //gets all info about article including related saves and comments
+    
+    router.get('/', (req, res) => {
+        db.Article
+            //find all the articles in the collection  
+            .find({})
+            //sort the articles by date scraped
+            .sort({dateScraped: -1})
+            //get another collection related to the articles
+            .populate('note')
+            //once you have all the info
+            .then(dbArticle => {
+                //send the info to index
+                res.render('', { data: dbArticle });
+            })
+            //if there is an error, send the error to the client
+            .catch(err => res.json(err));
+    });
+    
+    //save articles that you like 
+    router.post('/save/:id', function (req, res) {
+        db.Article.update(
+            { _id: req.params.id },
+            { $set: { saved: true } },
+            function (err, data) {
+                if (err) {
+                    throw err;
                 }
-            )
-        })
-        .catch(err => res.json(err));
-});
-
-// Remove the note
-app.post('/sub/:id1/:id2', function (req, res) {
-    db.Note.remove(
-        //remove the data for article note from the notes db
-        { _id: req.params.id2 },
-        (err, num) => {
-            if (err) throw err;
-        })
-        .then(dbNote => {
-            //make the noted in the db empty at that article id from the articles db
-            db.Article.update(
-                { _id: req.params.id1 },
-                { $unset: { note: '' } },
-                (err, num) => {
-                    if (err) throw err;
-                    res.send('Removed Note');
+                res.send("Saved!")
+            }
+        );
+    });
+    
+    //unsave articles 
+    router.post('/unsave/:id', function (req, res) {
+        db.Article.update(
+            { _id: req.params.id },
+            { $set: { saved: false } },
+            function (err, data) {
+                if (err) {
+                    throw err;
                 }
-            )
-        })
-        .catch(err => res.json(err));
+                res.send("Unsaved!")
+            }
+        );
+    });
+    
+    //add a comment/note
+    router.post('/add/:id', function (req, res) {
+        db.Note
+            //create note with body of the comment or note
+            .create(req.body)
+            //once you have created that, 
+            .then(dbNote => {
+                //update the Article db with the note collection
+                db.Article.update(
+                    { _id: req.params.id },
+                    { $set: { note: dbNote._id } },
+                    { multi: true },
+                    (err, num) => {
+                        if (err) throw err;
+                        res.send('Added');
+                    }
+                )
+            })
+            .catch(err => res.json(err));
+    });
+    
+    // Remove the note
+    router.post('/sub/:id1/:id2', function (req, res) {
+        db.Note.remove(
+            //remove the data for article note from the notes db
+            { _id: req.params.id2 },
+            (err, num) => {
+                if (err) throw err;
+            })
+            .then(dbNote => {
+                //make the noted in the db empty at that article id from the articles db
+                db.Article.update(
+                    { _id: req.params.id1 },
+                    { $unset: { note: '' } },
+                    (err, num) => {
+                        if (err) throw err;
+                        res.send('Removed Note');
+                    }
+                )
+            })
+            .catch(err => res.json(err));
+    
+    //Edit the note content
+    router.post('/edit/:id', function (req, res) {
+        db.Note.update(
+            { _id: req.params.id },
+            { $set: { text: req.body.text } },
+            (err, data) => {
+                if (err) throw err;
+                res.send('Edited Note');
+            }
+        )
+    });
+    });
+    
+module.exports = router;
 
-//Edit the note content
-app.post('/edit/:id', function (req, res) {
-    db.Note.update(
-        { _id: req.params.id },
-        { $set: { text: req.body.text } },
-        (err, data) => {
-            if (err) throw err;
-            res.send('Edited Note');
-        }
-    )
-});
-});
---
+++ public/scripts.js
@@ -0,0 +1,156 @@
+//Click to scrape news
+$('#new').on('click', () => {
+    scrapeArticle();
+  });
+  
+  //Click to save article
+  $('.save').on('click', function () {
+    saveArticle($(this).attr('data-id'));
+  });
+  
+  //Click to unsave article
+  $('.unsave').on('click', function () {
+    unsaveArticle($(this).attr('data-id'));
+  });
+  
+  //Click to add note
+  $('.plus').on('click', function () {
+    const id = $(this).attr('id').slice(4);
+    const $in = $(`#${id}`);
+    if ($in.val().trim() === '') {
+      $in.focus();
+      $in.val('');
+    } else {
+      addNote(id, $in.val());
+    }  
+  });
+  
+  //Click to delete note
+  $('.minus').on('click', function () {
+    const idArt = $(this).attr('id').slice(4);
+    const idNote = $(`#sub-${idArt}`).attr('data-id');
+    const $in = $(`#${idArt}`);
+    if ($(this).hasClass('edit')) {
+      if ($in.val().trim() === '') {
+        $in.focus();
+        $in.val('');
+      } else {
+        editNote(idNote, $in.val());
+      }    
+    } else {
+      delNote(idArt, idNote);
+    } 
+  });
+  
+  //Click editing note
+  $('.noted').on('click', function () {
+    const idArt = $(this).attr('id');
+    const idNote = $(`#sub-${idArt}`).attr('data-id');
+    addEdit(idArt, idNote);
+  });
+  
+  // Determine what to do if keys are pressed in input
+  $('input').on('keyup', function (e) {
+    const idArt = $(this).attr('id');
+    const idNote = $(`#sub-${idArt}`).attr('data-id');
+    if (e.keyCode === 13) {
+      if ($(`#add-${idArt}`).length !== 0) {
+        if ($(this).val().trim() === '') {
+          $(this).focus();
+          $(this).val('');
+        } else {
+          addNote(idArt, $(this).val());
+        }      
+      } else if ($(`#edit-${idArt}`).length !== 0) {
+        if ($(this).val().trim() === ''
+            || $(this).val().trim() === $(this).attr('value')) {
+          $(this).val($(this).attr('value'));
+          $(this).blur();
+          $(`#edit-${idArt}`).remove();
+        } else {
+          editNote(idNote, $(this).val());
+        }
+      }
+    } else {
+      if ($(`#edit-${idArt}`).length === 0) {
+        addEdit(idArt, idNote);
+      }    
+    }
+  });
+  
+  //Add Edit Button
+  const addEdit = (idArt, idNote) => {
+    const $in = $(`#${idArt}`);
+    if ($(`#edit-${idArt}`).length === 0) {
+      const $edit = $('<div>').addClass('edit').attr({
+        id: `edit-${idArt}`,
+        'data-id': idNote
+      }).html('✎').css('border', 'none');
+  
+      $edit.insertAfter($(`#sub-${idArt}`)).on('click', function () {
+        if ($in.val().trim() === ''
+            || $in.val().trim() === $in.attr('value')) {
+          $in.val($in.attr('value'));
+          $(this).remove();
+        } else {
+          editNote(idNote, $in.val());
+        }     
+      })
+    }
+  };
+  
+  /***** Separate ajax calls for future multiple use *****/
+  //Scrape Article
+  const scrapeArticle = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/scrape'
+    }).then( () => location.reload() );  
+  };
+  
+  //Save Article
+  const saveArticle = (id) => {
+    $.ajax({
+      method: 'POST',
+      url: `/save/${id}`,
+    }).then( () => location.reload() );
+  };
+  
+  //Unsave Article
+  const unsaveArticle = (id) => {
+    $.ajax({
+      method: 'POST',
+      url: `/unsave/${id}`,
+    }).then( () => location.reload() );
+  };
+  
+  //Add Note
+  const addNote = (id, text) => {
+    $.ajax({
+      method: 'POST',
+      url: `/add/${id}`,
+      data: {
+        text: text
+      }
+    }).then( () => location.reload() );
+  };
+  
+  //Delete Note
+  const delNote = (idArt, idNote) => {
+    $.ajax({
+      method: 'POST',
+      url: `/sub/${idArt}/${idNote}`
+    }).then( () => location.reload() );
+  };
+  
+  //Edit Note
+  const editNote = (idNote, text) => {
+    $.ajax({
+      method: 'POST',
+      url: `/edit/${idNote}`,
+      data: {
+        text: text
+      }
+    }).then( () => location.reload() );
+  };
+  
